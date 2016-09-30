411d730ef0fba50515135ffaf1befd87350f7b6d
filipraab <raabfilip@gmail.com>
2016-09-30 05:51:09 +0200

    dalsi verze

--- prepare.sh
+++ prepare.sh
@@ -2,4 +2,4 @@
 
 
 #spousti vsechny readky zacinajici #! v vimrc
-grep  "\"\!" .vimrc | cut -c 3- | while read line; do   eval $line; done
+grep  "\"\!" ~/.vimrc | cut -c 3- | while read line; do   eval $line; done
